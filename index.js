5e6a12c96b8da7b49a46545d5ed290756ff7567d
Olamilekan <olamilekan.akinlusi@icloud.com>
2025-04-03 14:47:21 -0500

    skinstric ai javascript

--- index.js
+++ index.js
@@ -11,5 +11,35 @@ innerBox.addEventListener("mouseleave", () => {
     outerBox.style.transform = "scale(1)";
 });
 
+window.onload = function() {
+    const inputField = document.querySelector('.where__form--text');
+    const proceedSection = document.querySelector('.analysis__front');
 
+    inputField.setAttribute("placeholder", "CLICK TO TYPE");
+    inputField.addEventListener('focus', function() {
+        inputField.setAttribute("placeholder", "WHERE ARE YOU FROM?");
+    });
+
+    inputField.addEventListener('input', function() {
+        if (inputField.value.trim() !== "") {
+            proceedSection.style.display = "inline-block";
+        } else {
+            proceedSection.style.display = "none";
+        }
+    });
+};
+
+document.addEventListener("DOMContentLoaded", function () {
+    const inputField = document.getElementById("userInput");
+    const proceedBtn = document.getElementById("proceedBtn");
+
+    function toggleProceedButton() {
+        if (inputField.value.trim() !== "") {
+            proceedBtn.classList.remove("disabled")
+        } else {
+           proceedBtn.classList.add("disabled")
+        }
+    }
+    proceedBtn.addEventListener("input", toggleProceedButton);
+});
   
